5e63ad7ce5a688fff43676c3fb8fc482342723bb
huzhengquan <huzhengquan@gmail.com>
2016-05-20 18:21:59 +0800

    style and CNAME

--- config.clj
+++ config.clj
@@ -2,7 +2,7 @@
   :input "./writer/"
 	:output "./"
 ;	:template "./template/"
-	:blog-name "kurrunk"
-  :blog-url "http://blog.kurrunk.com"
-  :blog-description "浆糊作用大"
+	:blog-name "浆糊糊"
+  :blog-url "http://huzhengquan.num.run"
+  :blog-description "环行在环行的环行中"
 }
