9fb24d2696330579ae3e54c88640e973a41e46d4
huzhengquan <huzhengquan@gmail.com>
2017-03-26 17:55:05 +0800

    new

--- _config.clj
+++ _config.clj
@@ -1,7 +1,7 @@
 {
   :input "./_writer/"
   :output "./"
-  :template "./templates/"
+  :template "./_templates/"
   :blog-name "孩子气"
   :blog-url "http://hzq.num.run"
   :blog-description "环行在环行的环行中环行"
